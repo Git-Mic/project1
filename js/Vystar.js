5789efc3593adedd6ecbf8585f9f34f98f82d684
unknown <overam23@gmail.com>
2024-02-13 20:10:26 -0500

    made images clickeable to ticketmaster from events

---
+++ js/Vystar.js
@@ -0,0 +1,37 @@
+const TPI_KEY = '53YF2UN6JBTNz0NB3UGOu9GjoEcoxCx7';
+// Vvenue ID for VyStar Veterans Memorial Arena
+const venueId = 'KovZpZAE67AA';
+
+const apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?apikey=${TPI_KEY}&venueId=${venueId}`;
+// Make the API call using fetch
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log("bugs bunny")
+    var events = data._embedded.events
+    for(var i = 0; i <events.length; i++){
+      console.log(events[i].name);
+      console.log(events[i].url);
+      console.log(events[i].dates.start.localDate);
+      console.log(events[i].dates.start.localTime);
+      console.log(events[i].images);
+      var eventContainer = document.createElement("div");
+      eventContainer.classList.add("event-container") ;
+      var title = document.createElement("h1");
+      var date = document.createElement("p");
+      var eventsImg = document.createElement("img");
+      date.textContent= events[i].dates.start.localDate;
+      title.textContent = events[i].name;
+      eventsImg.src = events[i].images[6].url;
+      document.body.appendChild(eventContainer)
+      eventContainer.appendChild(title);
+      eventContainer.appendChild(date);
+      eventContainer.appendChild(eventsImg);
+     
+      
+  }});
